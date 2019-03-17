19b0cca572388b7d3d44c30879a16c709361e0b4
kenji <knj.lau1@gmail.com>
2019-03-17 15:21:48 +0100

    first commit

--- contracts/Arena.sol
+++ contracts/Arena.sol
@@ -1,5 +1,45 @@
 pragma solidity ^0.4.24;
 
+import "./Farm.sol";
+
 contract Arena {
+
+    event NewFighter(address indexed owner, uint tokenId);
     
+    mapping (uint => bool) private _fighters;
+    mapping (uint => Fighter) private _fighterById;
+
+    Farm private _farm;
+
+    struct Fighter {
+        uint wins;
+        uint defeats;
+        uint reward;
+    }
+
+    constructor(Farm farm) public {
+        _farm = farm;
+    }
+
+    modifier isFighter(uint id) {
+        require(_fighters[id], "not a fighter");
+        _;
+    }
+
+    function registerFighter(uint id) public returns (bool) {
+        _preRegisterFighter(msg.sender, id);
+        _fighters[id] = true;
+        _fighterById[id] = Fighter(0, 0, 0);
+        emit NewFighter(msg.sender, id);
+        return true;
+    }
+
+    function _preRegisterFighter(address owner, uint id) private view {
+        require(_farm.isBreeder(owner), "not breeder");
+        require(owner == _farm.getOwnerOfAnimal(id), "not animal owner");
+        require(!_fighters[id], "already a fighter");
+    }
+
+    
+
 }
