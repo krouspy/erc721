5f5f5f8c16849ed5b033575a51b36d22a1fa5e30
krouspy <knjlau1@gmail.com>
2019-11-30 22:57:20 +0100

    change port

--- truffle-config.js
+++ truffle-config.js
@@ -1,27 +1,30 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
-
   networks: {
     development: {
-     host: "127.0.0.1",    
-     port: 7545,            
-     network_id: "*",       // Any network (default: none)
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Any network (default: none)
     },
 
     rinkeby: {
-      provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://rinkeby.infura.io/" + process.env.INFURA_API_KEY),
-      network_id: 1,       // Rinkeby's id
-      gas: 5500000,        
-      confirmations: 2,    
-      timeoutBlocks: 200,  
-      skipDryRun: true     
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://rinkeby.infura.io/" + process.env.INFURA_API_KEY
+        ),
+      network_id: 1, // Rinkeby's id
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
     }
   },
 
   compilers: {
     solc: {
-      version: "0.4.24",
+      version: "0.4.24"
     }
   }
-}
+};
